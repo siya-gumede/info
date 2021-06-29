b056360bff0a69c61758f40ddf6ef9202a564ccc
siya-gumede <gumede.siyabonga4@gmail.com>
2021-06-29 11:34:46 +0200

    Updates

---
+++ precache-manifest.6b17907eb48f20da4d8a282716269376.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29fc0c65b9c9c1666256fd5eee45e29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd56c16279e59b37da89",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd56c16279e59b37da89",
+    "url": "/static/js/main.9543becb.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "a9f4195e070ea88feecfdbd90af6a47c",
+    "url": "/static/media/resume.a9f4195e.pdf"
+  },
+  {
+    "revision": "a2dfbf49d776a708f18168c9de277ee4",
+    "url": "/static/media/siya.a2dfbf49.jpg"
+  }
+]);
